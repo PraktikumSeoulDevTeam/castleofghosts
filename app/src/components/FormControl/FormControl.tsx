d84cfae06c0ce904bf1c4b82928ef9668cdb12be
Nikita Ivanov <nikita@payme.uz>
2020-11-17 15:34:13 +0500

    update form control component;

--- app/src/components/FormControl/FormControl.tsx
+++ app/src/components/FormControl/FormControl.tsx
@@ -3,25 +3,22 @@ import {Formik, Form, Field, FormikErrors, FormikTouched} from 'formik';
 import {FormControlProps, FormControlFields, FormControlInputFields} from './types';
 import './FormControl.scss';
 
-function RenderFields(
+const RenderFields = (
     fields: FormControlFields,
     errors: FormikErrors<FormControlInputFields>,
     touched: FormikTouched<FormControlInputFields>
-) {
-    return Object.entries(fields).map(([fieldName, {placeholder, type, title}]) => {
-        return (
-            <div className="form-block__input-container" key={fieldName} style={{color: 'white'}}>
-                <label htmlFor={fieldName}>
-                    <div className="form-block__title">{title}</div>
-                    <Field placeholder={placeholder} className="form-block__input" type={type} name={fieldName} />
-                </label>
-                <div className="form-block__validation-message">
-                    {touched[fieldName] && errors[fieldName] ? errors[fieldName] : ''}
-                </div>
+) =>
+    Object.entries(fields).map(([fieldName, {placeholder, type, title}]) => (
+        <div className="form-block__input-container" key={fieldName} style={{color: 'white'}}>
+            <label htmlFor={fieldName}>
+                <div className="form-block__title">{title}</div>
+                <Field placeholder={placeholder} className="form-block__input" type={type} name={fieldName} />
+            </label>
+            <div className="form-block__validation-message">
+                {touched[fieldName] && errors[fieldName] ? errors[fieldName] : ''}
             </div>
-        );
-    });
-}
+        </div>
+    ));
 
 export function FormControl({fields, schema, onSubmit, children}: FormControlProps): JSX.Element {
     const initialValues: FormControlInputFields = Object.keys(fields).reduce((res, key) => {
