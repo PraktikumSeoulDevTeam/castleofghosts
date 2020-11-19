c47db35e0eeea6ecfb220a1ac98221920f9c1961
art4vm <69515971+art4vm@users.noreply.github.com>
2020-11-19 14:49:39 +0300

    Feature/52 refactoring (#13)
    * Основной шрифт заменен на 'Press Start 2P' (#55)
    
    * Рефакторинг (#52)
    
    Рефакторинг стилей
      Исправлены названия
      Приведены к camelCase параметры sass
      Нормализованы св-ва согласно правилам stylelint
      Вынесены в параметры URL assets
      Добавлены css св-ва с цветами
      Изменены единицы размеров шрифтов с px на rem
    
    Добавлено выполнение stylelint в pre-commit
    Поправлено описание в README

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
-import './assets/scss/app.scss';
+import './assets/scss/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
