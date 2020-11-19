3b613a54842d5660525d022ed30833eab5e109b6
art4vm <69515971+art4vm@users.noreply.github.com>
2020-11-19 14:49:39 +0300

    Feature/52 refactoring (#13)
    * Основной шрифт заменен на 'Press Start 2P' (#55)
    
    * Рефакторинг (#52)
    
    Рефакторинг стилей
      Исправлены названия
      Приведены к camelCase параметры sass
      Нормализованы св-ва согласно правилам stylelint
      Вынесены в параметры URL assets
      Добавлены css св-ва с цветами
      Изменены единицы размеров шрифтов с px на rem
    
    Добавлено выполнение stylelint в pre-commit
    Поправлено описание в README

--- app/src/components/copyright/Copyright.tsx
+++ app/src/components/copyright/Copyright.tsx
@@ -5,20 +5,20 @@ export function Copyright(): JSX.Element {
     return (
         <div className="copyright">
             <div className="copyright__title">Created by:</div>
-            <div className="copyright__names m--t__4">
-                <div className="copyright__name p--r__4 p--l__4">
+            <div className="copyright__names mt-4">
+                <div className="copyright__name px-4">
                     Tema
                     <div className="copyright__icon copyright__icon_tema" />
                 </div>
-                <div className="copyright__name p--r__4 p--l__4">
+                <div className="copyright__name px-4">
                     Nikita
                     <div className="copyright__icon copyright__icon_nikita" />
                 </div>
-                <div className="copyright__name p--r__4 p--l__4">
+                <div className="copyright__name px-4">
                     Dima
                     <div className="copyright__icon copyright__icon_dima" />
                 </div>
-                <div className="copyright__name p--r__4 p--l__4">
+                <div className="copyright__name px-4">
                     Jenya
                     <div className="copyright__icon copyright__icon_jenya" />
                 </div>
