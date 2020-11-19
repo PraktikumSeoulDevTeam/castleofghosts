0f2abbcf535a8691162c2165e8ef495863a2713e
Nikita Ivanov <nikita@payme.uz>
2020-11-19 12:58:59 +0500

    update Registration and Authorization pages;

--- app/src/pages/Authorization/Authorization.tsx
+++ app/src/pages/Authorization/Authorization.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as Yup from 'yup';
-import {Button} from '../../components/button/Button';
+import {Button} from '../../components/Button';
+import {FormControlFields} from '../../components/FormControl/types';
 import {FormControl} from '../../components/FormControl/FormControl';
 import './Authorization.scss';
 
@@ -15,27 +16,27 @@ const AuthorizationSchema = Yup.object().shape({
         .required('field must be required')
 });
 
+const authorizationField: FormControlFields = {
+    login: {
+        type: 'text',
+        placeholder: 'input your login',
+        title: 'Your login: '
+    },
+    password: {
+        type: 'password',
+        placeholder: 'input your password',
+        title: 'Your password: '
+    }
+};
+
 export function Authorization(): JSX.Element {
     return (
         <main className="ui">
             <div className="ui__inner authentication">
-                <h4 className="t-title authentication__title">
-                    <span>Authorization</span>
-                </h4>
+                <h1 className="t-title authentication__title">Authorization</h1>
                 <FormControl
                     schema={AuthorizationSchema}
-                    fields={{
-                        login: {
-                            type: 'text',
-                            placeholder: 'input your login',
-                            title: 'Your login: '
-                        },
-                        password: {
-                            type: 'password',
-                            placeholder: 'input your password',
-                            title: 'Your password: '
-                        }
-                    }}
+                    fields={authorizationField}
                     onSubmit={(formData) => {
                         // eslint-disable-next-line no-console
                         console.log(formData);
@@ -43,7 +44,7 @@ export function Authorization(): JSX.Element {
                 >
                     <footer className="authentication__footer">
                         <div>Registration</div>
-                        <Button className="btn t-main authorization-button" type="submit">
+                        <Button className="btn t-main authorization-button" submit>
                             Enter
                         </Button>
                     </footer>
--- app/src/pages/Registration/Registration.tsx
+++ app/src/pages/Registration/Registration.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import * as Yup from 'yup';
 import 'yup-phone';
-import {Button} from '../../components/button/Button';
+import {Button} from '../../components/Button';
+import {FormControlFields} from '../../components/FormControl/types';
 import {FormControl} from '../../components/FormControl/FormControl';
 import './Registration.scss';
 
@@ -14,47 +15,53 @@ const RegistrationSchema = Yup.object().shape({
         .min(4, 'min length 4 symbols')
         .max(16, 'max length 16 symbols')
         .required('field must be required'),
+    email: Yup.string().email(),
     phoneNumber: Yup.string().phone('*', false, 'phone number is not valid').required('field must be required'),
     firstName: Yup.string(),
     secondName: Yup.string()
 });
 
+const registrationFields: FormControlFields = {
+    login: {
+        type: 'text',
+        placeholder: 'Input your login',
+        title: 'Create new login: '
+    },
+    email: {
+        type: 'email',
+        placeholder: 'Input your email',
+        title: 'Input your email: '
+    },
+    phoneNumber: {
+        type: 'text',
+        placeholder: 'Input your phone',
+        title: 'Input your phone: '
+    },
+    firstName: {
+        type: 'text',
+        placeholder: 'Input your first name',
+        title: 'What is your first name: '
+    },
+    secondName: {
+        type: 'text',
+        placeholder: 'Input your second name',
+        title: 'What is your second name: '
+    },
+    password: {
+        type: 'password',
+        placeholder: 'Input your password',
+        title: 'Create new password: '
+    }
+};
+
 export function Registration(): JSX.Element {
     return (
         <main className="ui">
             <div className="ui__inner authentication">
-                <h4 className="t-title authentication__title">
-                    <span>Registration</span>
-                </h4>
+                <h1 className="t-title authentication__title">Registration</h1>
                 <FormControl
                     schema={RegistrationSchema}
-                    fields={{
-                        login: {
-                            type: 'text',
-                            placeholder: 'Input your login',
-                            title: 'Create new login: '
-                        },
-                        phoneNumber: {
-                            type: 'text',
-                            placeholder: 'Input your phone',
-                            title: 'Input your phone: '
-                        },
-                        firstName: {
-                            type: 'text',
-                            placeholder: 'Input your first name',
-                            title: 'What is your first name: '
-                        },
-                        secondName: {
-                            type: 'text',
-                            placeholder: 'Input your second name',
-                            title: 'What is your second name: '
-                        },
-                        password: {
-                            type: 'password',
-                            placeholder: 'Input your password',
-                            title: 'Create new password: '
-                        }
-                    }}
+                    fields={registrationFields}
                     onSubmit={(formData) => {
                         // eslint-disable-next-line no-console
                         console.log(formData);
