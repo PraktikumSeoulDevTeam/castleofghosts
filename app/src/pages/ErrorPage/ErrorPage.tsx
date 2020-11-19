f03b97fdfa52d5e21001de43f43a352fe5b0480d
Dima Kuznetcov <dimakuzn@gmail.com>
2020-11-19 18:58:10 +0300

    add page

--- app/src/pages/ErrorPage/ErrorPage.tsx
+++ app/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,4 +1,40 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import type {ErrorPageProps} from './types';
 
-export const ErrorPage = ({type}: ErrorPageProps): JSX.Element => <h3>{type}</h3>;
+export const ErrorPage = ({type}: ErrorPageProps): JSX.Element => {
+    let errorTxt = '';
+    switch (type) {
+        case '404':
+            errorTxt = 'Ничего не найдено';
+            break;
+        case '409':
+            errorTxt = 'У вас нет доступа';
+            break;
+        case '500':
+            errorTxt = 'Уппс, у нас произошла ошибка';
+            break;
+        default:
+            errorTxt = 'Что-то пошло не так!';
+    }
+
+    return (
+        <main className="error">
+            <div className="error__inner">
+                <div className="error__header">
+                    <h1 className="error__header-front">{type}</h1>
+                    <span className="error__header-back">{type}</span>
+                </div>
+                <div className="error__cnt">
+                    {errorTxt}
+                    <div className="mt-2" />
+                    Мы скоро всё исправим. <br />А пока можно начать с самого начала.
+                    <div className="mt-5" />
+                    <Link to="/" className="link">
+                        На старт
+                    </Link>
+                </div>
+            </div>
+        </main>
+    );
+};
