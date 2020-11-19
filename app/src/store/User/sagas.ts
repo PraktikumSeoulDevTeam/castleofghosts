19ab571d2253296a2b15e52c131615d8d7e07277
art4vm <69515971+art4vm@users.noreply.github.com>
2020-11-19 12:03:15 +0300

    Добавлена saga для регистрации (#43) (#11)

--- app/src/store/User/sagas.ts
+++ app/src/store/User/sagas.ts
@@ -1,12 +1,26 @@
 import {call, ForkEffect, put, takeLeading} from 'redux-saga/effects';
-import {getUserInfo, signIn, signOut} from '../../api';
-import {ApiUserInfo} from '../../api/types';
+import {getUserInfo, signUp, signIn, signOut} from '../../api';
 import {userRemoveAction, userUpdateAction} from './actions';
-import {SignInAction, USER_ACTION_TYPES} from './types';
+import {SignInAction, SignUpAction, USER_ACTION_TYPES} from './types';
+import type {ApiUserInfo} from '../../api/types';
 
 export function* userWatcher(): Generator<ForkEffect<never>> {
+    yield takeLeading(USER_ACTION_TYPES.SIGN_UP, userSignUpWorker);
     yield takeLeading(USER_ACTION_TYPES.SIGN_IN, userSignInWorker);
     yield takeLeading(USER_ACTION_TYPES.SIGN_OUT, userSignOutWorker);
+}
+
+function* userSignUpWorker(action: SignUpAction) {
+    try {
+        const isOk: boolean = yield call(signUp, action.payload);
+        if (isOk) {
+            const userInfo: ApiUserInfo = yield call(getUserInfo);
+            yield put(userUpdateAction(userInfo));
+        }
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log('[userSignUpWorker error] ', error);
+    }
 }
 
 function* userSignInWorker(action: SignInAction) {
