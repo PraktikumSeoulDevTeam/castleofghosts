15443e48fbb85afc6fd77f23ed5126cd9780f995
art4vm <69515971+art4vm@users.noreply.github.com>
2020-11-20 21:10:43 +0300

    Небольшие исправления настроек проекта
    Изменен параметр нормализации окончаний строк при checkin в git
    Теперь бинарные файлы игнорируются
    Возвращены рабочие спрайты для персонажей
    Исправлен регистр каталогов Button и Copyright
    Исправлен каталог сборки приложения
    Добавлен алиас для абсолютных путей в сборку webpack

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -5,12 +5,15 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: path.join(__dirname, 'src', 'index'),
     output: {
-        path: path.join(__dirname, '..', 'dist'),
+        path: path.join(__dirname, '..', 'dist', 'app'),
         filename: '[name].bundle.js',
         publicPath: '/'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.ts', '.tsx', '.js'],
+        alias: {
+            '~': path.resolve(__dirname, './src')
+        }
     },
     devtool: 'source-map',
     module: {
