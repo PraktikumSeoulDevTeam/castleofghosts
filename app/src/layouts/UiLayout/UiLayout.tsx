1ea00654b3edb119ab52bc8846936112631e8bd4
art4vm <69515971+art4vm@users.noreply.github.com>
2020-11-21 06:26:41 +0300

    Подготовка к верстке страниц
    Текущие стили UI вынесены в отдельный UiLayout
    Удалены неиспользуемые и лишние стили

---
+++ app/src/layouts/UiLayout/UiLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import type {UiLayoutProps} from './types';
+import './UiLayout.scss';
+
+export function UiLayout({children, isBlock, className}: UiLayoutProps): JSX.Element {
+    return (
+        <main className="ui pa-8">
+            <div className={`ui__container pa-5 ${isBlock ? 'ui__block' : ''} ${className || ''}`}>{children}</div>
+        </main>
+    );
+}
