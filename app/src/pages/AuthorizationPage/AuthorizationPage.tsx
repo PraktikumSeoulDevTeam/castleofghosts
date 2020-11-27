1c9e8350054f79a2f345e84a7d54fa476ea69a2e
art4vm <69515971+art4vm@users.noreply.github.com>
2020-11-27 04:14:37 +0300

    Исправлены недочеты в отображении форм (#29)

--- app/src/pages/AuthorizationPage/AuthorizationPage.tsx
+++ app/src/pages/AuthorizationPage/AuthorizationPage.tsx
@@ -11,22 +11,19 @@ import type {FormControlFields} from '../../components/FormControl/types';
 import type {ApiSignInRequest} from '../../api/types';
 
 const AuthorizationSchema = Yup.object().shape({
-    login: Yup.string().required('field must be required').max(12, 'max length 12 symbols'),
-    password: Yup.string()
-        .min(4, 'min length 4 symbols')
-        .max(16, 'max length 16 symbols')
-        .required('field must be required')
+    login: Yup.string().required('field is required').max(12, 'max length 12 symbols'),
+    password: Yup.string().min(4, 'min length 4 symbols').max(16, 'max length 16 symbols').required('field is required')
 });
 
 const authorizationField: FormControlFields = {
     login: {
         type: 'text',
-        placeholder: 'input your login',
+        placeholder: 'Login',
         title: 'Your login: '
     },
     password: {
         type: 'password',
-        placeholder: 'input your password',
+        placeholder: 'Password',
         title: 'Your password: '
     }
 };
@@ -48,12 +45,12 @@ const connecter = connect(null, mapDispatchToProps);
 
 function AuthorizationComponent({onAuth}: ConnectedProps<typeof connecter>): JSX.Element {
     return (
-        <UiLayout isBlock className="authentication">
-            <h1 className="t-title authentication__title">Authorization</h1>
+        <UiLayout isBlock>
+            <h1 className="t-title">Authorization</h1>
             <FormControl schema={AuthorizationSchema} fields={authorizationField} onSubmit={onAuth}>
-                <footer className="authentication__footer">
-                    <Link to="/registration" className="mr-5">
-                        Registration
+                <footer className="button-bar mt-5">
+                    <Link to="/registration">
+                        <Button className="btn btn_txt">Registration</Button>
                     </Link>
                     <Button type="submit">Enter</Button>
                 </footer>
