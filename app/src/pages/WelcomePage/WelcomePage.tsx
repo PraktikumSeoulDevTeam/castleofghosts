135161626145b02282a8d9442481a159829a874a
art4vm <69515971+art4vm@users.noreply.github.com>
2020-11-29 14:00:06 +0300

    Добавлен компонент меню игры (#34)
    Компонент WelcomePage был преобразован в меню.
    Упрощена навигация. Теперь после авторизации достаточно перейти в
     корневой роут.
    Удалены настройки для обратной навигации в PrivateRoute
    В PrivateRoute добавлен параметр publicExclusive для роутов, которые
    доступны только неавторизованным пользователям

--- app/src/pages/WelcomePage/WelcomePage.tsx
+++ app/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {Button, Copyright} from '../../components';
+import {Copyright, Menu} from '../../components';
 import {UiLayout} from '../../layouts';
-import './WelcomePage.scss';
 
 export function WelcomePage(): JSX.Element {
     return (
-        <UiLayout isBlock className="welcome-page">
-            <div className="welcome-page__title-block">
-                <h1 className="t-title">
-                    <div>Castle</div>
-                    <div>of</div>
-                    <div>Ghosts</div>
-                </h1>
-                <Link to="/game" className="t-center mt-5">
-                    <Button>Start</Button>
-                </Link>
-                <Link to="/leaderboard" className="t-center mt-5">
-                    <span>Leaderboard</span>
-                </Link>
-            </div>
-            <div>
+        <UiLayout isBlock>
+            <h1 className="welcome-page__title t-title mt-10">
+                <div>Castle</div>
+                <div>of</div>
+                <div>Ghosts</div>
+            </h1>
+            <Menu className="welcome-page__menu mt-5" />
+            <div className="mt-10">
                 <Copyright />
             </div>
         </UiLayout>
