c94066a80759a9e44087c563f4a545fa89bda8be
Nikita <FellonOne@users.noreply.github.com>
2020-12-01 20:55:25 +0500

    feature/spinner-on-auth (#32)
    * add withLoading HOC;
    add Spinner;
    update Authorization and Registration pages, added spinner after sending
    requests;
    
    * update logic;
    remove HOC;
    updates sagas;
    
    * fix issues in pr;
    
    * Update app/src/layouts/UiLayout/UiLayout.tsx
    
    * Update app/src/pages/LeaderboardPage/LeaderboardPage.tsx
    
    Co-authored-by: Nikita Ivanov <nikita@payme.uz>
    Co-authored-by: art4vm <69515971+art4vm@users.noreply.github.com>

--- app/src/store/rootReducer.ts
+++ app/src/store/rootReducer.ts
@@ -2,5 +2,11 @@ import {combineReducers} from 'redux';
 import {userReducer} from './User/reducer';
 import {gameReducer} from './Game/reducer';
 import {leaderboardReducer} from './Leaderboard/reducer';
+import {utilityReducer} from './Utility/reducer';
 
-export const rootReducer = combineReducers({user: userReducer, game: gameReducer, leaderboard: leaderboardReducer});
+export const rootReducer = combineReducers({
+    user: userReducer,
+    game: gameReducer,
+    leaderboard: leaderboardReducer,
+    utility: utilityReducer
+});
