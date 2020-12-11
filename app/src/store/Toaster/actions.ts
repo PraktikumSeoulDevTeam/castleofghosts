89bf297c2aad47191110aab7474679ac17008ed7
art4vm <69515971+art4vm@users.noreply.github.com>
2020-12-11 13:58:23 +0300

    Update actions.ts

--- app/src/store/Toaster/actions.ts
+++ app/src/store/Toaster/actions.ts
@@ -1,26 +1,11 @@
 import {ToasterAddAction, TOASTER_ACTION_TYPES, ToasterAction, Toast} from './types';
 
-export function toasterAddAction({text, duration, id}: Toast): ToasterAddAction {
+export function toasterAddAction(toast: Toast): ToasterAddAction {
     return {
         type: TOASTER_ACTION_TYPES.ADD_TOASTER,
-        payload: {
-            id,
-            text,
-            duration: duration ?? 1000
-        }
+        payload: toast
     };
 }
-
-/* export function toasterCreateAction({text, duration, id}: Toast): ToasterCreateAction {
-    return {
-        type: TOASTER_ACTION_TYPES.CREATE_TOASTER,
-        payload: {
-            text,
-            id,
-            duration: duration ?? 1000
-        }
-    };
-} */
 
 export function toasterRemoveAction(id: number): ToasterAction {
     return {
