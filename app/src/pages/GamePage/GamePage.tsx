ec9db43fe46a586dd3f7364e00f0cbce753a36b4
art4vm <69515971+art4vm@users.noreply.github.com>
2020-12-12 08:30:38 +0300

    Обнуление состояние игры
    Добавлено обнуление текущего состояния игры при навигации со страницы игры
    Для синхронизации использован history.block

--- app/src/pages/GamePage/GamePage.tsx
+++ app/src/pages/GamePage/GamePage.tsx
@@ -1,4 +1,7 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
+import {useHistory} from 'react-router-dom';
+import {connect, ConnectedProps} from 'react-redux';
+import {gameRemoveAction} from '../../store/Game/actions';
 import {GameUi} from './GameUi/GameUi';
 import {setBgCanvas} from '../../core/bg.canvas';
 import {setMainCanvas} from '../../core/main.canvas';
@@ -8,21 +11,36 @@ import './GamePage.scss';
 const width = 1024;
 const height = 768;
 
-export const GamePage = (): JSX.Element => {
-    const bgCanvas = useRef<HTMLCanvasElement>();
-    const mainCanvas = useRef<HTMLCanvasElement>();
-    React.useEffect(() => {
-        setBgCanvas(bgCanvas.current);
-        setMainCanvas(mainCanvas.current);
-    }, []);
-
-    return (
-        <UiLayout className="game-page">
-            <div className="game-page__container" style={{width: width + 4, height: height + 50}}>
-                <canvas ref={bgCanvas} width={width} height={height} className="game-page__bg" />
-                <canvas ref={mainCanvas} width={width} height={height} className="game-page__main" />
-                <GameUi className="game-page__ui" />
-            </div>
-        </UiLayout>
-    );
+const mapDispatch = {
+    gameRemove: gameRemoveAction
 };
+
+const connector = connect(null, mapDispatch);
+
+export const GamePage = connector(
+    ({gameRemove}: ConnectedProps<typeof connector>): JSX.Element => {
+        const bgCanvas = useRef<HTMLCanvasElement>();
+        const mainCanvas = useRef<HTMLCanvasElement>();
+        const history = useHistory();
+
+        useEffect(() => {
+            setBgCanvas(bgCanvas.current);
+            setMainCanvas(mainCanvas.current);
+            const unblock = history.block(() => {
+                gameRemove();
+            });
+
+            return () => unblock();
+        }, []);
+
+        return (
+            <UiLayout className="game-page">
+                <div className="game-page__container" style={{width: width + 4, height: height + 50}}>
+                    <canvas ref={bgCanvas} width={width} height={height} className="game-page__bg" />
+                    <canvas ref={mainCanvas} width={width} height={height} className="game-page__main" />
+                    <GameUi className="game-page__ui" />
+                </div>
+            </UiLayout>
+        );
+    }
+);
