2adb2ff803c1bdba74635e2ad5592cca6cb33cf1
Ivanov Nikita <fellonbit96@gmail.com>
2021-01-15 13:03:04 +0500

    remove dead code;

--- app/src/store/selectors/currentLevel.ts
+++
@@ -1,9 +0,0 @@
-import {store} from '..';
-
-import {Level} from '../Level/types';
-
-export function selectCurrentLevel(): Level {
-    const levelId = store.getState().game.level.number ?? 0;
-
-    return store.getState().level.levels[levelId];
-}
