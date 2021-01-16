47afe816ef7246acc2e550f75509bcfbc46e21ba
art4vm <69515971+art4vm@users.noreply.github.com>
2021-01-16 23:30:25 +0300

    Исправление ошибки при остановке аудио ноды

--- app/src/core/audio/types.ts
+++ app/src/core/audio/types.ts
@@ -2,3 +2,8 @@ export interface SampleControl {
     sample: 'CLICK' | 'GAME_LOOP' | 'WIN' | 'LOOSE';
     action: 'PLAY' | 'STOP';
 }
+
+export interface SampleContainer {
+    node: AudioBufferSourceNode;
+    startTime?: number;
+}
